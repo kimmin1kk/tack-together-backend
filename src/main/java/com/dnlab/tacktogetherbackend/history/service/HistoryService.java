53bcbff71da815d7be817595c8bf4614a173e123
kimmin1kk <101302590+kimmin1kk@users.noreply.github.com>
2023-04-16 13:54:31 +0900

    feat: History 패키지 초기 구조 생성

---
+++ src/main/java/com/dnlab/tacktogetherbackend/history/service/HistoryService.java
@@ -0,0 +1,9 @@
+package com.dnlab.tacktogetherbackend.history.service;
+
+import com.dnlab.tacktogetherbackend.history.dto.HistoryDto;
+
+import java.util.List;
+
+public interface HistoryService {
+
+}
