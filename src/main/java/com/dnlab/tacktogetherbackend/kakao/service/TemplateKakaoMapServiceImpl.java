e508fa7fffb7d45ae882625c829ddc2e02352fdf
Kang <Kang815595@gmail.com>
2023-04-19 16:28:31 +0900

    refactor: 외부 API 조회 방식 변경
    - RestTemplate 를 통한 외부 API 조회를 사용하였으나 해당 방식은 장려되지 않음으로, WebClient 를 통한 조회로 변경

--- src/main/java/com/dnlab/tacktogetherbackend/kakao/service/TemplateKakaoMapServiceImpl.java
+++ src/main/java/com/dnlab/tacktogetherbackend/kakao/service/TemplateKakaoMapServiceImpl.java
@@ -8,11 +8,9 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-@Service
 @RequiredArgsConstructor
 public class TemplateKakaoMapServiceImpl implements KakaoMapService {
     private final RestTemplate restTemplate;
--- src/main/java/com/dnlab/tacktogetherbackend/kakao/service/WebFluxKakaoMapServiceImpl.java
+++ src/main/java/com/dnlab/tacktogetherbackend/kakao/service/WebFluxKakaoMapServiceImpl.java
@@ -4,9 +4,11 @@ import com.dnlab.tacktogetherbackend.kakao.common.dto.RequestDirections;
 import com.dnlab.tacktogetherbackend.kakao.common.dto.responsedirection.ResponseDirections;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Slf4j
+@Service
 @RequiredArgsConstructor
 public class WebFluxKakaoMapServiceImpl implements KakaoMapService {
     private final WebClient webClient;
