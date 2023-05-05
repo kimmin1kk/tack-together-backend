5204d32b4b8081b2bf9cabc8352b2f52fbba0b8e
Kang <Kang815595@gmail.com>
2023-05-05 20:10:46 +0900

    refactor: 매칭요청 성공 시 전송되는 payload 변경
    Service 객체 외부에서는 MatchRequest Id만 반환되도록 변경됨. 서버에서 클라이언트로 필요한 정보만 JSON 에 담아 반환되도록 변경

--- src/main/java/com/dnlab/tacktogetherbackend/match/controller/MatchController.java
+++ src/main/java/com/dnlab/tacktogetherbackend/match/controller/MatchController.java
@@ -3,13 +3,13 @@ package com.dnlab.tacktogetherbackend.match.controller;
 import com.dnlab.tacktogetherbackend.match.common.MatchDecisionStatus;
 import com.dnlab.tacktogetherbackend.match.common.MatchRequest;
 import com.dnlab.tacktogetherbackend.match.dto.MatchRequestDTO;
+import com.dnlab.tacktogetherbackend.match.dto.MatchResultInfoDTO;
 import com.dnlab.tacktogetherbackend.match.dto.UserResponse;
 import com.dnlab.tacktogetherbackend.match.service.MatchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import java.security.Principal;
+import java.util.Map;
 import java.util.Objects;
 
 @Slf4j
@@ -39,34 +40,35 @@ public class MatchController {
         log.debug("매칭 요청 수신");
         // DTO 로부터 MatchRequest 객체를 생성하고 맵에 추가
         matchRequestDTO.setUsername(principal.getName());
-        final MatchRequest matchRequest = matchService.addMatchRequest(matchRequestDTO);
-        Objects.requireNonNull(headerAccessor.getSessionAttributes()).put(MATCH_REQUEST_ID, matchRequest.getId());
+        String matchRequestId = matchService.addMatchRequest(matchRequestDTO);
+        Objects.requireNonNull(headerAccessor.getSessionAttributes()).put(MATCH_REQUEST_ID, matchRequestId);
 
         // 매칭 조건에 맞는 매칭 요청 찾기
-        MatchRequest matchedMatchRequest = matchService.findMatchingMatchRequests(matchRequest);
+        String opponentMatchRequestId = matchService.findMatchingMatchRequests(matchRequestId);
 
         // 매칭 조건이 맞으면 각 사용자들에게 매칭 정보 전송
-        if (matchedMatchRequest != null) {
+        if (!opponentMatchRequestId.isBlank()) {
             log.info("Match Succeed!");
-            log.info("matched match request info : " + matchedMatchRequest);
-            matchRequest.setMatchedMatchRequestId(matchedMatchRequest.getId());
-            matchedMatchRequest.setMatchedMatchRequestId(matchRequest.getId());
 
-            matchService.handlePendingMatched(matchRequest, matchedMatchRequest);
-            messagingTemplate.convertAndSendToUser(matchedMatchRequest.getUsername(), DESTINATION_URL, matchRequest);
-            messagingTemplate.convertAndSendToUser(matchRequest.getUsername(), DESTINATION_URL, matchedMatchRequest);
+            // 매칭 결과 생성
+            Map<String, MatchResultInfoDTO> resultInfoDTOMap = matchService.getMatchResultInfos(matchRequestId, opponentMatchRequestId);
+
+            // 매칭 결과를 각각 전송
+            messagingTemplate.convertAndSendToUser(resultInfoDTOMap.get(opponentMatchRequestId).getUsername(), DESTINATION_URL, resultInfoDTOMap.get(opponentMatchRequestId));
+            messagingTemplate.convertAndSendToUser(principal.getName(), DESTINATION_URL, resultInfoDTOMap.get(opponentMatchRequestId));
         }
     }
 
     // 매칭 수락 처리
+    // 결합도가 높음
     @MessageMapping("/match/accept")
-    public void handleAccept(@Payload String matchedRequestId, SimpMessageHeaderAccessor headerAccessor) {
+    public void handleAccept(SimpMessageHeaderAccessor headerAccessor) {
         String matchRequestId = (String) Objects.requireNonNull(headerAccessor.getSessionAttributes()).get(MATCH_REQUEST_ID);
         log.info(matchRequestId);
         MatchRequest matchRequest = matchService.getMatchRequestById(matchRequestId).orElseThrow();
-        MatchRequest matchedRequest = matchService.getMatchRequestById(matchRequest.getMatchedMatchRequestId()).orElseThrow();
+        MatchRequest matchedRequest = matchService.getMatchRequestById(matchRequest.getOpponentMatchRequestId()).orElseThrow();
 
-        MatchDecisionStatus status = matchService.acceptMatch(matchRequest);
+        MatchDecisionStatus status = matchService.acceptMatch(matchRequestId);
 
         if (status.equals(MatchDecisionStatus.ACCEPTED)) {
             sendAcceptedMessage(matchRequest, matchedRequest);
@@ -77,12 +79,12 @@ public class MatchController {
 
     // 매칭 거절 처리
     @MessageMapping("/match/reject")
-    public void handleReject(@Payload String matchedRequestId, SimpMessageHeaderAccessor headerAccessor) {
+    public void handleReject(SimpMessageHeaderAccessor headerAccessor) {
         String matchRequestId = (String) Objects.requireNonNull(headerAccessor.getSessionAttributes()).get(MATCH_REQUEST_ID);
         MatchRequest matchRequest = matchService.getMatchRequestById(matchRequestId).orElseThrow();
-        MatchRequest matchedRequest = matchService.getMatchRequestById(matchRequest.getMatchedMatchRequestId()).orElseThrow();
+        MatchRequest matchedRequest = matchService.getMatchRequestById(matchRequest.getOpponentMatchRequestId()).orElseThrow();
 
-        matchService.rejectMatch(matchRequest);
+        matchService.rejectMatch(matchRequestId);
 
         UserResponse response = new UserResponse(MatchDecisionStatus.REJECTED.toString());
         messagingTemplate.convertAndSendToUser(matchRequest.getUsername(), DESTINATION_URL, response);
