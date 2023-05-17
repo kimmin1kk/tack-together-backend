6010ba52b8a9bd970825df3b32c1975cc0163f72
Kang <Kang815595@gmail.com>
2023-05-17 19:13:24 +0900

    refactor: 웹소켓 메시지 헤더의 키 값을 컨트롤러에서 application.yml으로 이동
    - MatchController에 필드로 있던 headerEventType을 application.yml로 옮김
    - MatchedController의 동승시작 요청 메소드를 제거

--- src/main/java/com/dnlab/tacktogetherbackend/matched/controller/MatchedController.java
+++ src/main/java/com/dnlab/tacktogetherbackend/matched/controller/MatchedController.java
@@ -5,13 +5,17 @@ import com.dnlab.tacktogetherbackend.matched.dto.LocationUpdateRequestDTO;
 import com.dnlab.tacktogetherbackend.matched.service.MatchedService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.support.GenericMessage;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
 
 @Slf4j
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -23,23 +27,27 @@ public class MatchedController {
 
     private static final String DESTINATION_URL = "/queue/matched";
 
+    @Value("${stomp.header-event-type}")
+    private String headerEventType;
+
     @MessageMapping("/matched/shareLocation")
     public void handleSharingLocation(Principal principal,
                                       @Payload LocationUpdateRequestDTO locationUpdateRequestDTO) {
         String opponentUsername = matchedService.getOpponentUsernameBySessionId(locationUpdateRequestDTO.getSessionId(), principal.getName());
         LocationInfoResponseDTO locationInfoResponseDTO = matchedService.handleLocationUpdate(locationUpdateRequestDTO, principal.getName());
-        messagingTemplate.convertAndSendToUser(opponentUsername, DESTINATION_URL, locationInfoResponseDTO);
+
+
+        Map<String, Object> headers = Collections.singletonMap(headerEventType, "request");
+        messagingTemplate.convertAndSendToUser(opponentUsername, DESTINATION_URL, new GenericMessage<>(locationInfoResponseDTO, headers));
 
         // 동승 시작 시 사용자에게도 메시지 전송
         if (locationInfoResponseDTO.isRidingStarted()) {
-            messagingTemplate.convertAndSendToUser(principal.getName(), DESTINATION_URL, LocationInfoResponseDTO.builder()
+            messagingTemplate.convertAndSendToUser(principal.getName(), DESTINATION_URL, new GenericMessage<>(LocationInfoResponseDTO.builder()
                     .sessionId(locationUpdateRequestDTO.getSessionId())
                     .ridingStarted(true)
-                    .build());
+                    .build(), headers));
         }
     }
 
-    @MessageMapping("/matched/startRiding")
-    public void handleStartRiding(Principal principal) {
-    }
+
 }
