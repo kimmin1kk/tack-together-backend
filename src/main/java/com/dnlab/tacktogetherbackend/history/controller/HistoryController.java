dbdf9a51c009d2af3b0e07706b9a9d8aff1c6489
kimmin1kk <101302590+kimmin1kk@users.noreply.github.com>
2023-05-22 20:59:36 +0900

    refactor: 맵핑 어노테이션 변경

--- src/main/java/com/dnlab/tacktogetherbackend/history/controller/HistoryController.java
+++ src/main/java/com/dnlab/tacktogetherbackend/history/controller/HistoryController.java
@@ -1,11 +1,10 @@
 package com.dnlab.tacktogetherbackend.history.controller;
 
-import com.dnlab.tacktogetherbackend.history.dto.HistoriesDTO;
-import com.dnlab.tacktogetherbackend.history.dto.HistoryDTO;
+import com.dnlab.tacktogetherbackend.history.dto.SimpleHistoryDTO;
+import com.dnlab.tacktogetherbackend.history.dto.DetailHistoryDTO;
 import com.dnlab.tacktogetherbackend.history.service.HistoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -18,16 +17,15 @@ public class HistoryController {
     //누르면 최근 한달간 볼 수 있고 전체보기를 누르면 다 볼 수 있게
     private final HistoryService historyService;
 
-    @MessageMapping("/history/simple")
+    @GetMapping("/simple")
     //이용기록 간단하게
-    public ResponseEntity<HistoriesDTO> handleSimpleHistoryRequest(Principal principal) {
-        return ResponseEntity.ok(historyService.findHistoriesByUserName(principal.getName()));
+    public ResponseEntity<SimpleHistoryDTO> handleSimpleHistoryRequest(Principal principal) {
+        return ResponseEntity.ok(historyService.getSimpleHistoryByUsername(principal.getName()));
     }
-
     //이용기록 자세하게
-    @MessageMapping("history/detail")
-    public ResponseEntity<HistoryDTO> handleDetailHistoryRequest(Principal principal) {
-        return ResponseEntity.ok(historyService.findHistoryByUsername(principal.getName()));
+    @GetMapping("detail")
+    public ResponseEntity<DetailHistoryDTO> handleDetailHistoryRequest(Principal principal) {
+        return ResponseEntity.ok(historyService.getDetailHistoryByUsername(principal.getName()));
     }
 
 }
