606a8c067f2242370872ed61d9f439e426199c9e
Kang <Kang815595@gmail.com>
2023-05-24 02:56:17 +0900

    refactor: 로깅 스타일 변경

--- src/main/java/com/dnlab/tacktogetherbackend/match/common/NoSuchMatchRequestException.java
+++ src/main/java/com/dnlab/tacktogetherbackend/match/common/NoSuchMatchRequestException.java
@@ -4,7 +4,7 @@ import java.util.NoSuchElementException;
 
 public class NoSuchMatchRequestException extends NoSuchElementException {
     public NoSuchMatchRequestException() {
-        super("해당 매칭요청을 찾을 수 없습니다.");
+        super("찾을 수 없는 매칭정보 아이디");
     }
 
     public NoSuchMatchRequestException(String s) {
